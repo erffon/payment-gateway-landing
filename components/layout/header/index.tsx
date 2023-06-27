47b8ef7ef872d543ced2aadeac48aa57354a7d39
erfan <erffon@gmail.com>
2023-06-27 11:39:56 +0330

    Feat: Custom Fonts + Header Links

--- components/layout/header/index.tsx
+++ components/layout/header/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import logo from "./assets/logo.png";
+import style from "./header.module.css";
 
 const CHeader = () => {
   return (
@@ -7,14 +8,14 @@ const CHeader = () => {
       className="overflow-hidden flex justify-between py-5 mb-10 bg-slate-black"
       dir="rtl"
     >
-      <div className="relative w-20 h-20">
-        <Image src={logo} alt="website logo" fill />
+      <div className="relative">
+        <Image src={logo} alt="website logo" width={80} height={80} />
       </div>
       <div className="flex justify-center items-center">
-        <ul className="flex">
-          <li>خانه</li>
-          <li>درباره ما</li>
-          <li>ارتباط با ما</li>
+        <ul className="flex justify-around py-4 backdrop-blur-lg bg-zinc-700 rounded-full border-zinc-400/80 border w-96">
+          <li className={style.links}>خانه</li>
+          <li className={style.links}>درباره</li>
+          <li className={style.links}>ارتباط با من</li>
         </ul>
       </div>
       <div className="flex justify-center items-center">
