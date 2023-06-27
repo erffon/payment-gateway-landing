71c987222e2fafbe9bfffb28510e99e2fad89db2
erfan <erffon@gmail.com>
2023-06-27 17:59:04 +0330

    Fix: logo linked to home

--- components/layout/header/index.tsx
+++ components/layout/header/index.tsx
@@ -2,13 +2,16 @@ import Image from "next/image";
 import logo from "./assets/logo.png";
 import style from "./header.module.css";
 import { headerContent } from "../../../contents/homepage/data";
+import Link from "next/link";
 
 const CHeader = () => {
   return (
     <header className={style.container} dir="rtl">
-      <div className="relative">
-        <Image src={logo} alt="website logo" width={80} height={80} />
-      </div>
+      <Link href="/">
+        <div className="relative">
+          <Image src={logo} alt="website logo" width={80} height={80} />
+        </div>
+      </Link>
       <div className="flex justify-center items-center">
         <ul className={style["links-container"]}>
           {headerContent.links.map((item) => {
